386eead6bb7728346fcf6f35feb1c5af63c21dab
heinrich-marx <weriast98@gmail.com>
2021-07-02 19:54:04 +0300

    init: func max, min avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  }
+  else {
+    let m = 0;
+    for (let i = 0; i<array.length;i++) {
+      if (m > array[i]) {
+        m = array[i]
+      }
+    }
+    return m;
+  }
 }
 
+
+
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined|| array.length == 0) {
+    return 0;
+  }
+  else {
+    let m = 0;
+    for (let i = 0; i<array.length;i++) {
+      if (m < array[i]) {
+        m = array[i]
+      }
+    }
+    return m;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array == undefined || array.length == 0) {
+    return 0;
+  }
+  else {
+    let m = array[0];
+    for (let i = 1; i<array.length;i++) {
+      m += array[i]
+    }
+    let a = m/array.length;
+    return a.toFixed(2)
+  }
 }
